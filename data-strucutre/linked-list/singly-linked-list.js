6eb7960abe0e4125f177816a8049ad1fdd8d0cad
pronazmul <developernazmul@gmail.com>
2023-09-11 07:54:19 +0600

    Singly-linked-list done

--- data-strucutre/linked-list/singly-linked-list.js
+++ data-strucutre/linked-list/singly-linked-list.js
@@ -1,3 +1,10 @@
+class Node {
+  constructor(value) {
+    this.value = value
+    this.next = null
+  }
+}
+
 class singlyLinkList {
   constructor() {
     this.head = null
@@ -9,8 +16,8 @@ class singlyLinkList {
     return this.length === 0
   }
 
-  in(data) {
-    let newData = { value: data, next: null }
+  insertLast(data) {
+    let newData = new Node(data)
 
     if (this.isEmpty()) {
       this.head = newData
@@ -22,12 +29,79 @@ class singlyLinkList {
 
     this.length++
   }
+
+  removeLast() {
+    if (!this.head) return null
+
+    if (this.length === 1) {
+      let removed = this.head
+      this.head = null
+      this.tail = null
+      this.length = 0
+    }
+
+    let current = this.head
+    let last = this.tail
+    let newLast
+
+    while (current) {
+      if (current.next == last) {
+        newLast = current
+        break
+      }
+      current = current.next
+    }
+
+    let removed = last
+
+    newLast.next = null
+    this.tail = newLast
+    this.length--
+  }
+
+  removeFirst() {
+    if (!this.head) return null
+
+    if (this.length === 1) {
+      let removed = this.head
+      this.head = null
+      this.tail = null
+      this.length = 0
+    }
+
+    this.head = this.head.next
+    this.length--
+  }
+
+  insertFirst(data) {
+    let newData = new Node(data)
+    if (this.isEmpty()) {
+      this.head = newData
+      this.tail = newData
+    } else {
+      newData.next = this.head
+      this.head = newData
+    }
+    this.length++
+  }
+
+  getList() {
+    let arr = []
+    if (this.isEmpty()) return arr
+
+    let currentNode = this.head
+    while (currentNode) {
+      arr.push(currentNode.value)
+      currentNode = currentNode.next
+    }
+    return arr
+  }
 }
 
 let list = new singlyLinkList()
-list.in(5)
-list.in(10)
-list.in(15)
-list.in(20)
-list.in(25)
-console.log({ list })
+list.insertLast(5)
+list.insertLast(10)
+list.insertFirst(30)
+list.insertFirst(200)
+
+console.log({ list: list.getList() })
